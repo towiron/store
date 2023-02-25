42292fa78fb1ee0dee16c71404988fd2d10b7ac0
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-02-26 04:34:37 +0500

    added logic for login form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+from .models import User
+from .forms import UserLoginForm
+
 
 def login(request):
-	return render(request, 'users/login.html')
+	if request.method == 'POST':
+		form = UserLoginForm(data=request.POST)
+		if form.is_valid():
+			username = request.POST['username']
+			password = request.POST['password']
+			user = auth.authenticate(username=username, password=password)
+			if user:
+				auth.login(request, user)
+				return redirect('index')
+	else:
+		form = UserLoginForm()
+	context = {'form': form}
+	return render(request, 'users/login.html', context)
 
 
 

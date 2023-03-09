bb6d57992f99d00fcca5109d1dce59f0e20a45d0
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-03-09 10:20:46 +0500

    logic for total_sum and total_quantity

--- users/views.py
+++ users/views.py
@@ -48,10 +48,17 @@ def profile(request):
 			print(form.errors)
 	else:
 		form = UserProfileForm(instance=request.user)
+
+	baskets = Basket.objects.filter(user=request.user)
+	# total_sum = sum(basket.sum() for basket in baskets)
+	# total_quantity = sum(basket.quantity for basket in baskets)
+
 	context = {
 		'title': 'Store - Профиль',
 	    'form': form,
-	    'baskets': Basket.objects.filter(user=request.user)
+	    'baskets': baskets,
+	    # 'total_sum': total_sum,
+	    # 'total_quantity': total_quantity,
 		}
 	return render(request, 'users/profile.html', context)
 

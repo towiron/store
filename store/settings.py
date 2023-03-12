3642d0a9aec3e8c47893da49b5a6c5d7e3e9d4e8
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-03-12 17:18:35 +0500

    connecting yandex to send verification email

--- store/settings.py
+++ store/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from data import email_data
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,4 +140,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Sending emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = email_data.email
+EMAIL_HOST_PASSWORD = email_data.password
+EMAIL_USE_SSL = True

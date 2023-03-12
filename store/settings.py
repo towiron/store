9ba933b0e55d5fd242aa73a8ccfc4f062241561a
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-03-12 16:32:21 +0500

    email verification

--- store/settings.py
+++ store/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DOMAIN_NAME = 'http://localhost:8000'
 
 # Application definition
 
@@ -137,3 +138,6 @@ LOGIN_URL = '/users/login/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Sending emails
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
-from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from common.views import TitleMixin
 
-from .models import User
+from .models import User, EmailVerification
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
@@ -39,3 +41,19 @@ class UserProfileView(TitleMixin, UpdateView):
 		context = super(UserProfileView, self).get_context_data()
 		context['baskets'] = Basket.objects.filter(user=self.object)
 		return context
+	
+
+class EmailVerificationView(TitleMixin, TemplateView):
+	title = 'Store - Подтверждение электронной почты'
+	template_name = 'users/email_verification.html'
+
+	def get(self, request, *args, **kwargs):
+		code = kwargs['code']
+		user = User.objects.get(email=kwargs['email'])
+		email_verifications = EmailVerification.objects.filter(user=user, code=code)
+		if email_verifications.exists() and not email_verifications.first().is_expired():
+			user.is_verified_email = True
+			user.save()
+			return super(EmailVerificationView, self).get(request, *args, **kwargs)
+		else:
+			return HttpResponseRedirect(reverse('index'))
